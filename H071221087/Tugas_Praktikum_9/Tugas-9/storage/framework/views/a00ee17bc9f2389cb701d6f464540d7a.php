f0f816944965d1e23d263bfedf4651834ab3187e
IQBAL <mhmmad.iqbal75@gmail.com>
2023-12-11 07:46:11 +0800

    menghapus folder tugas 9

---
+++ H071221087/Tugas_Praktikum_9/Tugas-9/storage/framework/views/a00ee17bc9f2389cb701d6f464540d7a.php
@@ -0,0 +1,95 @@
+<?php $__env->startSection('content'); ?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php $__env->startSection('title'); ?>
+        Edit Hero
+    <?php $__env->stopSection(); ?>
+    <style>
+        .registration-box {
+            width: 500px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            border: 1px solid;
+            border-radius: 10px;
+            padding: 20px 80px 30px;
+            margin: 40px auto;
+        }
+
+        .registration-head h3 {
+            margin: 0;
+            padding-bottom: 20px;
+        }
+
+        .registration-body {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .registration-body input {
+            margin-bottom: 20px;
+            height: 40px;
+            width: 250px;
+        }
+
+        .registration-body button {
+            background-color: rgb(29, 174, 255);
+            color: black;
+            border-radius: 5px;
+            position: relative;
+            right: 100;
+            width: 100px;
+            height: 35px;
+        }
+
+        .menubtn {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+
+        .menubtn a {
+            display: flex;
+            align-items: center;
+            width: 100px;
+            height: 40px;
+        }
+
+        .menubtn input {
+            background-color: rgb(29, 174, 255);
+            border-radius: 5px;
+            width: 100px;
+            height: 40px;
+            margin-bottom: 20px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="registration-box">
+        <div class="registration-head">
+            <h3>Edit Karakter</h3>
+        </div>
+        <div>
+            <form action="/updatehero/<?php echo e($data->id); ?>" method="post">
+                <?php echo csrf_field(); ?>
+                <div class="registration-body">
+                    <input type="text" name="karakter" placeholder="karakter" value="<?php echo e($data->karakter); ?>" required>
+                    <input type="text" name="role" placeholder="Role" value="<?php echo e($data->role); ?>" required>
+                    <input type="text" name="tipe" placeholder="Tipe" value="<?php echo e($data->tipe); ?>" required>
+                    <input type="text" name="asal" value="<?php echo e($data->asal); ?>" placeholder="asal">
+                    <input type="text" name="deskripsi" value="<?php echo e($data->deskripsi); ?>" placeholder="deskripsi">
+                </div>
+                <div class="menubtn">
+                    <a href="/">Back</a>
+                    <input id="submit" type="submit" value="Update"></input>
+                </div>
+        </div>
+        </form>
+    </div>
+    <?php $__env->stopSection(); ?>
+    </div>
+
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH E:\Kuliah\SEMESTER 3\Pemrograman Web\Praktikum Web\Tugas Praktikum\Tugas_Praktikum_9\Tugas-9\resources\views/edithero.blade.php ENDPATH**/ ?>
